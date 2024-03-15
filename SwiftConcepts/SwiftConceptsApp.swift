55eb0be2c1f0af93c1b0b8f3a0ef1a56cca60ca4
Joe <jgliniecki@gmail.com>
2024-03-15 13:08:57 -0500

    Initial Commit

---
+++ SwiftConcepts/SwiftConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConceptsApp.swift
+//  SwiftConcepts
+//
+//  Created by Joseph Gliniecki on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
