0b4e7520e5447f8d5a8d7c35ff669a92e1806aad
Joe <jgliniecki@gmail.com>
2025-01-23 14:24:04 -0600

    Initial Commit

---
+++ SwiftConcepts/SwiftConcepts.h
@@ -0,0 +1,18 @@
+//
+//  SwiftConcepts.h
+//  SwiftConcepts
+//
+//  Created by Joseph Gliniecki on 1/23/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftConcepts.
+FOUNDATION_EXPORT double SwiftConceptsVersionNumber;
+
+//! Project version string for SwiftConcepts.
+FOUNDATION_EXPORT const unsigned char SwiftConceptsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftConcepts/PublicHeader.h>
+
+
