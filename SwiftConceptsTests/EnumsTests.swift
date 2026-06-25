3c03bba1656c39fb3101319f222b4fa5f784440a
Joe <jgliniecki@gmail.com>
2026-06-25 11:27:40 -0500

    Clarify examples

--- SwiftConceptsTests/EnumsTests.swift
+++ SwiftConceptsTests/EnumsTests.swift
@@ -1,64 +1,51 @@
-import XCTest // Used for all unit testing
-//@testable import SwiftConcepts // This imports our SwiftConcepts project code into the test
+import XCTest
 
-final class EnumsTests: XCTestCase { // XCTestCase is the class used for unit testing
-  
-  // Enums are the same as structs except we know all the cases at runtime.
-  
-  func testStruct() {
-    struct CarModel {
-      let brand: CarBrand
-      let model: String
-    }
-    struct CarBrand {
-      let title: String
-    }
+final class EnumsTests: XCTestCase {
+
+    // Enums are a set of named values we can use in our code
     
-    var brand1 = CarBrand(title: "Ford")
-    var brand2 = CarBrand(title: "Toyota")
-    
-    // Struct: Brand can change at runtime
-    var car1 = CarModel(brand: brand1, model: "Fiesta")
-    var car2 = CarModel(brand: brand1, model: "Focus")
-    var car3 = CarModel(brand: brand2, model: "Camry")
-  }
-  
-  func testEnum() {
-    struct CarModel {
-      let brand: CarBrandOption
-      let model: String
-    }
-    
-    // Enum: here we limit the options to ford and toyota
-    enum CarBrandOption {
-      case ford
-      case toyota
-      case honda, chevy // Can also define on one row
-      
-      // Enums can have computed variables
-      var title: String {
-        // Switch is useful because compiler complains if you don't include all the cases
-        switch self {
-        case .ford:
-          return "Ford"
-        case .toyota:
-          return "Toyota"
-        case .chevy:
-          return "Chevy"
-        case .honda:
-          return "Honda"
-        default:
-          return "Default"
+    func testCreateEnum() {
+        enum TempUnit {
+            case celsius
+            case fahrenheit
+            case kelvin
         }
         
-      }
+        // Can also do one line
+        enum Weekday {
+            case monday, tuesday, wednesday, thursday, friday
+        }
     }
     
-    var car1 = CarModel(brand: CarBrandOption.ford, model: "Fiesta")
-    var car2 = CarModel(brand: CarBrandOption.ford, model: "Focus")
-    var car3 = CarModel(brand: CarBrandOption.toyota, model: "Camry")
+    func testAssignEnum() {
+        enum TempUnit {
+            case celsius, fahrenheit, kelvin
+        }
+        
+        var unit = TempUnit.kelvin
+        unit = .celsius // Don't need TempUnit prefix since unit variable is already of type TempUnit
+        XCTAssertTrue(unit == .celsius)
+    }
     
-    XCTAssertEqual(car1.brand.title, "Ford")
-    
-  }
+    func testEnumComputedValues() {
+        enum TempUnit {
+            case celsius, fahrenheit, kelvin
+            
+            // Enums can have read-only computed property
+            var abbreviation: String {
+                // Switch is useful because compiler complains if you don't include all the cases
+                switch self {
+                case .celsius:
+                    return "C"
+                case .fahrenheit:
+                    return "F"
+                case .kelvin:
+                    return "K"
+                }
+            }
+        }
+            
+        XCTAssertEqual(TempUnit.celsius.abbreviation, "C")
+        
+    }
 }
